2d5fffc1d00f5bd85349dd83e8eaf4f9cd73be7a
Eduardo Barros <eduardo.santos@acbprocessamento.com.br>
2022-07-25 15:49:14 -0300

    add e configurando core do sistema para atender multiplaforma

--- LibraryToDocs/LibraryToDocs.API/Program.cs
+++ LibraryToDocs/LibraryToDocs.API/Program.cs
@@ -1,4 +1,10 @@
+using LibraryToDocs.Configurations;
+using LibraryToDocs.Data;
+using LibraryToDocs.Service.FileDataService;
+using LibraryToDocs.Service.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,6 +32,50 @@ builder.Services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo
     }
 }));
 
+
+//-- AutoMapper -->
+//builder.Services.AddAutoMapper(typeof(Service.ClassMapper));
+
+// database..
+var databaseService = CoreSettings.GetService(CoreSettings.Core);
+switch (databaseService.Repository.Provider)
+{
+    case "SqlServer":
+        builder.Services.AddDbContext<CoreDbContext>(options =>
+            options.UseSqlServer(databaseService.Repository.ConnectionString));
+        break;
+    case "MySql":
+        builder.Services.AddDbContext<CoreDbContext>(options =>
+            options.UseMySql(databaseService.Repository.ConnectionString,
+                new MySqlServerVersion(new Version(8, 0, 21)))); // Replace with your server version and type.
+        break;
+    default:
+        throw new Exception("DatabaseType not configured");
+}
+// filedata
+var appSettings = CoreSettings.GetSettings();
+switch (appSettings.FileDataSettings.FileDataStorage)
+{
+    case EFileDataStorage.AWS:
+        builder.Services.AddScoped<IStorageService, AWSStorage>();
+        break;
+    case EFileDataStorage.Azure:
+        builder.Services.AddScoped<IStorageService, AzureStorage>();
+        break;
+    case EFileDataStorage.CGP:
+        builder.Services.AddScoped<IStorageService, GoogleCloudStorage>();
+        break;
+    default:
+        builder.Services.AddScoped<IStorageService, LocalStorage>();
+        break;
+}
+
+
+var assembly = AppDomain.CurrentDomain.Load("Core.Service");
+//builder.Services.AddMediatR(assembly);
+
+// configure jwt authentication
+var key = System.Text.Encoding.UTF8.GetBytes(appSettings.Jwt.Key);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
