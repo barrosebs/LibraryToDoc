656e8da21a4bba334e97397f88c9e2f46adf1dd6
Eduardo Barros <eduardo.santos@acbprocessamento.com.br>
2022-07-27 08:35:49 -0300

    configurando Swagger

--- LibraryToDocs/LibraryToDocs.WebApi/Program.cs
+++ LibraryToDocs/LibraryToDocs.WebApi/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,24 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo
+{
+    Version = "v1",
+    Title = "DeliveryToDocs :: API",
+    Description = "Gestão, Controle e Idenxação de Documentos",
+    TermsOfService = new Uri("https://example.com/terms"),
+    Contact = new OpenApiContact
+    {
+        Name = "Eduardo Barros",
+        Email = "barrosebs@gmail.com",
+        Url = new Uri("https://twitter.com/spboyer"),
+    },
+    License = new OpenApiLicense
+    {
+        Name = "EBDeveloper",
+        Url = new Uri("https://example.com/license"),
+    }
+}));
 
 var app = builder.Build();
 
