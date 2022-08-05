8fa581ee5d7c465db1001bce47dd27ae19bdf0a8
Eduardo Barros <eduardo.santos@acbprocessamento.com.br>
2022-08-05 16:39:42 -0300

    adicionando projeto frontLogin

---
+++ FrontEnd/LibraryToDocs-Web/src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http'
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PrincipalComponent } from './pages/compartilhado/principal/principal.component';
+import { AppRoutingModule } from './app-routing-module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import {MatCardModule} from '@angular/material/card'
+import {MatInputModule} from '@angular/material/input'
+import {MatButtonModule} from '@angular/material/button'
+import {MatSidenavModule} from '@angular/material/sidenav'
+import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatIconModule} from '@angular/material/icon'
+import {MatSnackBarModule} from '@angular/material/snack-bar'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    PrincipalComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSnackBarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
